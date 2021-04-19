81716cea1bfc44522320588752748753c801e0c9
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-04-18 22:14:31 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,47 +1,60 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from mpl_toolkits.mplot3d import Axes3D
 
-#Function for getting r(important for calculating the slope of regression line)
-def get_r(x_array, y_array):
-	numerator = 0
-	dem1 = 0
-	dem2 = 0
-	for i in range(len(x_array)):
-		numerator += (x_array[i] - np.mean(x_array)) * (y_array[i] - np.mean(y_array))
-		dem1 += (x_array[i] - np.mean(x_array))**2
-		dem2 += (y_array[i] - np.mean(y_array))**2
-	denom = np.sqrt(dem1 * dem2)
-	return numerator / denom
-
-#Function for calulcating the slope of the regression line
-def calculate_slope(x_array, y_array):
-	return get_r(x_array, y_array) * (np.std(y_array)/np.std(x_array))
-
-#Calculating the y_intercept of the regression line
-def y_intercept(x_array, y_array):
-	slope = calculate_slope(x_array, y_array)
-	return np.mean(y_array) - slope * np.mean(x_array)
-
-#Using the y=mx+b equation and the other functions I programmed to predict y based on x
-def predict_y(x_array, y_array, theX):
-	slope = calculate_slope(x_array, y_array)
-	y_inter = y_intercept(x_array, y_array)
-	return slope*theX + y_inter
-
-#Create the Linear Regression class using the predict_y function. Class inspired based on sklearn's LinearRegression function
 class ManualLinearRegression:
-	def __init__(self, xArray = None, yArray = None):
-		self.xArray = xArray
-		self.yArray = yArray
-	def fit(x_array, y_array):
-		self.xArray = x_array
-		self.yArray = y_array
-	def predict(theNum):
-		return predict_y(self.xArray, self.yArray, theNum)
-  
-#Test out manually made Linear Regression
-x_array = [17,13,12,15,16,14,16,16,18,19]
-y_array = [94,73,59,80,93,85,66,79,77,91]
+    def __init__(self, learning_rate=0.01, max_iter=1000, param_vector=None):
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+        self.param_vector = param_vector
+        
+    def fit(self, X, y):
+        X = [np.insert(x, 0, 1) for x in X]
+        
+        self.param_vector = np.random.randn(len(X[0]), 1)
+        
+        for x in range(self.max_iter):
+            for j in range(len(self.param_vector)):
+                hyps = np.dot(X, self.param_vector)
+                
+                gradients = []
+                for i in range(len(hyps)):
+                    gradient = (hyps[i] - y[i]) * X[i][j]
+                    gradients.append(gradient)
+                cost_der = self.learning_rate * np.mean(gradients)
+                
+                self.param_vector[j] -= cost_der
+        
+    def predict(self, X):
+        X = [np.insert(x, 0, 1) for x in X]
+        prediction = np.dot(X, self.param_vector)
+        return prediction
 
-lr = ManualLinearRegression()
-lr.fit(x_array, y_array)
-print(lr.predict(21))
+    def score(self, X, y):
+        r2score = r2_score(y, self.predict(X))
+        return r2score
+
+
+X1, X2 = np.linspace(0, 10, 200), np.random.randint(0, 10, 200)
+X = [[X1[i], X2[i]] for i in range(len(X1))]
+y = np.linspace(0, 5, 200)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+for i in range(len(X)):
+    ax.scatter(X[i][0], X[i][1], y[i])
+
+model = ManualLinearRegression()
+model.fit(X, y)
+
+X_mesh, Y_mesh = np.meshgrid(X, y)
+params = model.param_vector
+Z = params[0] + params[1]*X_mesh + params[2]*Y_mesh
+
+ax.plot_surface(X_mesh, Y_mesh, Z)
+ax.set(xlabel='x1', ylabel='x2', zlabel='y', title='Multiple Linear Regression')
+
+plt.show()
